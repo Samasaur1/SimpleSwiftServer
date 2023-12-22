23c288e22146cfc6d08884f6776c541c4e369d8a
Abhishek Biswas <128672478+abhishekbiswas772@users.noreply.github.com>
2023-12-22 12:00:00 +0530

    Added Secure Functionlity

--- Sources/SimpleSwiftServer/main.swift
+++ Sources/SimpleSwiftServer/main.swift
@@ -4,6 +4,8 @@ import Foundation.NSFileManager
 import SwiftIP
 import ArgumentParser
 
+private let sensitiveFilePatterns: [String] = ["*.key", "id_rsa", "*.env", "*.pem", "*.ppk"]
+
 extension String {
     public var fileName: String {
         return URL(fileURLWithPath: self).lastPathComponent
@@ -31,23 +33,71 @@ struct Server: ParsableCommand {
         case fileDownload = "file"
         case webServer = "webserver"
         case HTMLFile = "html"
-
+        
         static func name(for value: Server.Mode) -> NameSpecification {
             return .customLong(value.rawValue)
         }
     }
-
+    
     @Option(name: .long, help: "The port for the server to run on") var port: UInt16 = 1234
     @Flag(help: "The server mode") var mode: Mode = .directoryBrowser
     @Argument(help: "The path to the file/directory") var path: String = "."
     @Flag(name: .long, help: "Whether or not to log incoming requests") var debug = false
+    
+    
+    
+    /// Checking For sensitiveFilePatterns
+    private func isFileSensitive(_ fileName: String, patterns: [String]) -> Bool {
+        let fileExtension = URL(fileURLWithPath: fileName).pathExtension
+        let baseName = URL(fileURLWithPath: fileName).deletingPathExtension().lastPathComponent
 
+        for pattern in patterns {
+            if pattern.contains("*.") {
+                let extensionPattern = pattern.replacingOccurrences(of: "*.", with: "")
+                if fileExtension == extensionPattern {
+                    return true
+                }
+            } else if baseName == pattern {
+                return true
+            }
+        }
+        return false
+    }
+    
+    private func decodeAndCleanPath(_ path: String, basePath: String) -> String {
+        let trimmedPath = path.hasPrefix(basePath) ? String(path.dropFirst(basePath.count)) : path
+        return trimmedPath.removingPercentEncoding ?? trimmedPath
+    }
+
+    /// Wrapper Func For customDirectoryBrowser
+
+    private func customDirectoryBrowser(basePath: String, originalDirectoryBrowser: @escaping (String) -> ((HttpRequest) -> HttpResponse)) -> ((HttpRequest) -> HttpResponse) {
+        return { request in
+            let requestedPath = decodeAndCleanPath(request.path, basePath: "/files/").trimmingCharacters(in: .whitespaces)
+            let fullPath = (basePath as NSString).appendingPathComponent(requestedPath)
+            var isDir: ObjCBool = false
+            if FileManager.default.fileExists(atPath: fullPath, isDirectory: &isDir) {
+                if isDir.boolValue {
+                    return originalDirectoryBrowser(requestedPath)(request)
+                } else if isFileSensitive(requestedPath, patterns: sensitiveFilePatterns) {
+                    return .forbidden
+                } else {
+                    print(originalDirectoryBrowser(requestedPath)(request))
+                    return originalDirectoryBrowser(requestedPath)(request)
+                }
+            } else {
+                return .notFound
+            }
+        }
+    }
+
+    
     func run() throws {
         let server = HttpServer()
-
+        
         switch mode {
         case .directoryBrowser:
-            server["/files/:path"] = directoryBrowser(path)
+            server["/files/:path"] = customDirectoryBrowser(basePath: path, originalDirectoryBrowser: directoryBrowser)
             server["/"] = { _ in
                 return .movedTemporarily("files/")
             }
@@ -81,14 +131,14 @@ struct Server: ParsableCommand {
                 return .movedTemporarily("page/")
             }
         }
-
+        
         if debug {
             server.middleware.append { req in
                 print("\(req.method) request from \(req.address ?? "unknown address") to \(req.path)")
                 return nil
             }
         }
-
+        
         let semaphore = DispatchSemaphore(value: 0)
         do {
             try server.start(port, forceIPv4: true)
